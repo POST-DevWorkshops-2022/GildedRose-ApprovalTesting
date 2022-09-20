32a51ac5cce7bee7706ee0298d81fb5cb8d34584
Katrin Heiderer <katrin.heiderer@nagarro.com>
2022-09-20 10:30:48 +0200

    Add Approval Tests

--- CSharpCore/CSharpCoreTest/GildedRoseTest.cs
+++ CSharpCore/CSharpCoreTest/GildedRoseTest.cs
@@ -1,9 +1,14 @@
 using Xunit;
 using System.Collections.Generic;
 using FluentAssertions;
+using ApprovalTests.Reporters;
+using ApprovalTests;
+using Newtonsoft.Json;
+using ApprovalTests.Combinations;
 
 namespace CSharpCore.Test
 {
+    [UseReporter(typeof(VisualStudioReporter))]
     public class GildedRoseTest
     {
         [Fact]
@@ -16,5 +21,56 @@ namespace CSharpCore.Test
             Items[0].Name.Should().Be("foo");
         }
 
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void OvertimeCalculationToString()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Approvals.Verify(Items[0].ToString());
+        }
+
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void OvertimeCalculationJson()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Approvals.Verify(JsonConvert.SerializeObject(Items[0]));
+        }
+
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void OvertimeCalculationVerifyJson()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Approvals.VerifyJson(JsonConvert.SerializeObject(Items[0]));
+        }
+
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void OvertimeCalculationCombinations()
+        {
+            string[] names = { "foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", };
+            int[] quality = { 0, 1, 49, 50 };
+            int[] sellIn = { -1, 0, 2, 6, 11 };
+
+            CombinationApprovals.VerifyAllCombinations(doUpdateQuality, names, sellIn, quality);
+        }
+
+        private string doUpdateQuality(string name, int sellIn, int quality)
+        {
+            Item[] items = new Item[] { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            return JsonConvert.SerializeObject(items[0]);
+        }
     }
 }
